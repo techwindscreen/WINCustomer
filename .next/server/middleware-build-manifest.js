02faebb16986cc68fba48e18f932565aed76efac
Your Name <mhal236@uky.edu>
2025-08-13 17:28:19 +0800

    new repo

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -10,11 +10,6 @@ self.__BUILD_MANIFEST = {
   "rootMainFiles": [],
   "rootMainFilesTree": {},
   "pages": {
-    "/": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/index.js"
-    ],
     "/_app": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
@@ -30,15 +25,15 @@ self.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/contact-info.js"
     ],
-    "/damage-location": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/damage-location.js"
-    ],
     "/payment-success": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/payment-success.js"
+    ],
+    "/quote": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/quote.js"
     ]
   },
   "ampFirstPages": []
